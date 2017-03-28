4cb2d722fe6ac039a8ae59760ce3c580f36d21c4
sfujiwara <shuhei.fujiwara@gmail.com>
2017-03-28 12:21:15 +0900

    add reuse option and use tf.get_variable in fully connected layers

--- tfmodel/vgg16.py
+++ tfmodel/vgg16.py
@@ -14,9 +14,7 @@ VGG_MEAN = [123.68, 116.779, 103.939]
 
 def _vgg_conv2d(inputs, filters):
     n_kernels = inputs.get_shape()[3].value
-    # w = tf.Variable(tf.truncated_normal([3, 3, n_kernels, filters], stddev=0.1), name="weights")
     w = tf.get_variable("weights", [3, 3, n_kernels, filters], tf.float32, tf.random_normal_initializer())
-    # b = tf.Variable(tf.zeros([filters]), name="biases")
     b = tf.get_variable("biases", [filters], tf.float32, tf.zeros_initializer())
     h = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(inputs, w, strides=[1, 1, 1, 1], padding="SAME"), b))
     return h, w, b
@@ -27,9 +25,9 @@ class Vgg16:
     def __init__(self):
         pass
 
-    def build_graph(self, img_tensor):
+    def build_graph(self, img_tensor, reuse=False):
         # Convolution layers 1
-        with tf.variable_scope("conv1"):
+        with tf.variable_scope("conv1", reuse=reuse):
             with tf.variable_scope("conv1_1"):
                 h_conv1_1, w_conv1_1, b_conv1_1 = _vgg_conv2d(img_tensor, filters=64)
             with tf.variable_scope("conv1_2"):
@@ -37,7 +35,7 @@ class Vgg16:
         # Pooling 1
         pool1 = tf.nn.max_pool(h_conv1_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME", name="pool1")
         # Convolution layers 2
-        with tf.variable_scope("conv2"):
+        with tf.variable_scope("conv2", reuse=reuse):
             with tf.variable_scope("conv2_1"):
                 h_conv2_1, w_conv2_1, b_conv2_1 = _vgg_conv2d(pool1, filters=128)
             with tf.variable_scope("conv2_2"):
@@ -45,7 +43,7 @@ class Vgg16:
         # Pooling 2
         pool2 = tf.nn.max_pool(h_conv2_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME", name="pool2")
         # Convolution layers 3
-        with tf.variable_scope("conv3"):
+        with tf.variable_scope("conv3", reuse=reuse):
             with tf.variable_scope("conv3_1"):
                 h_conv3_1, w_conv3_1, b_conv3_1 = _vgg_conv2d(pool2, filters=256)
             with tf.variable_scope("conv3_2"):
@@ -55,7 +53,7 @@ class Vgg16:
         # Pooling 3
         pool3 = tf.nn.max_pool(h_conv3_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME", name="pool3")
         # Convolution 4
-        with tf.variable_scope("conv4"):
+        with tf.variable_scope("conv4", reuse=reuse):
             with tf.variable_scope("conv4_1"):
                 h_conv4_1, w_conv4_1, b_conv4_1 = _vgg_conv2d(pool3, filters=512)
             with tf.variable_scope("conv4_2"):
@@ -65,7 +63,7 @@ class Vgg16:
         # Pooling 4
         pool4 = tf.nn.max_pool(h_conv4_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME", name="pool4")
         # Convolution 5
-        with tf.variable_scope("conv5"):
+        with tf.variable_scope("conv5", reuse=reuse):
             with tf.variable_scope("conv5_1"):
                 h_conv5_1, w_conv5_1, b_conv5_1 = _vgg_conv2d(pool4, filters=512)
             with tf.variable_scope("conv5_2"):
@@ -75,19 +73,19 @@ class Vgg16:
         # Pooling 5
         pool5 = tf.nn.max_pool(h_conv5_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME", name="pool5")
         # Fully connected 6
-        with tf.variable_scope("fc6"):
-            w_fc6 = tf.Variable(tf.truncated_normal([7, 7, 512, 4096], stddev=0.1), name="weights")
-            b_fc6 = tf.Variable(tf.zeros([4096]), name="biases")
+        with tf.variable_scope("fc6", reuse=reuse):
+            w_fc6 = tf.get_variable("weights", [7, 7, 512, 4096], tf.float32, tf.random_normal_initializer())
+            b_fc6 = tf.get_variable("biases", [4096], tf.float32, tf.zeros_initializer())
             h_fc6 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(pool5, w_fc6, strides=[1, 1, 1, 1], padding="VALID"), b_fc6))
         # Fully connected 7
-        with tf.variable_scope("fc7"):
-            w_fc7 = tf.Variable(tf.truncated_normal([1, 1, 4096, 4096], stddev=0.1), name="weights")
-            b_fc7 = tf.Variable(tf.zeros([4096]), name="biases")
+        with tf.variable_scope("fc7", reuse=reuse):
+            w_fc7 = tf.get_variable("weights", [1, 1, 4096, 4096], tf.float32, tf.random_normal_initializer())
+            b_fc7 = tf.get_variable("biases", [4096], tf.float32, tf.zeros_initializer())
             h_fc7 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(h_fc6, w_fc7, strides=[1, 1, 1, 1], padding="SAME"), b_fc7))
         # Fully connected 8
-        with tf.variable_scope("fc8"):
-            w_fc8 = tf.Variable(tf.truncated_normal([1, 1, 4096, 1000], stddev=0.1), name="weights")
-            b_fc8 = tf.Variable(tf.zeros([1000]), name="biases")
+        with tf.variable_scope("fc8", reuse=reuse):
+            w_fc8 = tf.get_variable("weights", [1, 1, 4096, 1000], tf.float32, tf.random_normal_initializer())
+            b_fc8 = tf.get_variable("biases", [1000], tf.float32, tf.zeros_initializer())
             h_fc8 = tf.nn.bias_add(tf.nn.conv2d(h_fc7, w_fc8, strides=[1, 1, 1, 1], padding="SAME"), b_fc8)
         with tf.variable_scope("prob"):
             self.logits = h_fc8
