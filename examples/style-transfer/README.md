b831633062829530c12f12e6335ecac96d57f533
sfujiwara <shuhei.fujiwara@gmail.com>
2017-04-18 04:37:18 +0900

    implement neural style transfer

---
+++ examples/style-transfer/README.md
@@ -0,0 +1,19 @@
+# Neural Algorithm of Artistic Style with TensorFlow
+
+An implementation of "[A Neural Algorithm of Artistic Style](https://arxiv.org/abs/1508.06576)" with TensorFlow.
+
+## Requirements
+
+* TensorFlow
+* SciPy
+
+## How to Run
+
+```
+git clone https://github.com/sfujiwara/tfmodel.git
+cd examples/style-transfer
+```
+
+```
+python style_transfer.py
+```
--- examples/style-transfer/style_transfer.py
+++ examples/style-transfer/style_transfer.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 import numpy as np
 from scipy.misc import imread, imresize, imsave
 import tensorflow as tf
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)))
+
 import tfmodel
 
-content_img = np.array([imresize(imread("examples/img/tensorflow_logo.png", mode="RGB"), [224, 224])], dtype=np.float32)
-style_img = np.array([imresize(imread("examples/img/chouju_sumou.jpg", mode="RGB"), [224, 224])], dtype=np.float32)
+CONTENT_WEIGHT = 1.
+STYLE_WEIGHT = 100.
+LEARNING_RATE = 1.
+
+content_img = np.array([imresize(imread("img/tensorflow_logo.png", mode="RGB"), [224, 224])], dtype=np.float32)
+style_img = np.array([imresize(imread("img/chouju_sumou.jpg", mode="RGB"), [224, 224])], dtype=np.float32)
 
 with tf.Graph().as_default() as g1:
     img_ph = tf.placeholder(tf.float32, [1, 224, 224, 3])
@@ -20,7 +29,6 @@ with tf.Graph().as_default() as g1:
 
 with tf.Graph().as_default() as g2:
     img_tensor = tf.Variable(tf.random_normal([1, 224, 224, 3]))
-    # img_tensor = tf.Variable(content_img)
     net = tfmodel.vgg.Vgg16(img_tensor=img_tensor, trainable=False)
     content_layer_tensors = [net.h_conv4_2, net.h_conv5_2]
     style_layer_tensors = [net.h_conv1_1, net.h_conv2_1, net.h_conv3_1, net.h_conv4_1, net.h_conv5_1]
@@ -34,10 +42,21 @@ with tf.Graph().as_default() as g2:
         content_loss = tf.reduce_sum(content_losses)
     # Define style loss
     with tf.name_scope("style_loss"):
+        style_losses = []
         for i in range(len(style_layers)):
-            # TODO: compute style loss
-            pass
-    optim = tf.train.AdamOptimizer(learning_rate=1e-1).minimize(content_loss)
+            # Compute target gram matrix
+            features = np.reshape(style_layers[i], (-1, style_layers[i].shape[3]))
+            style_gram = np.matmul(features.T, features) / features.size
+            # Define style tensor
+            _, height, width, number = map(lambda x: x.value, style_layer_tensors[i].get_shape())
+            size = height * width * number
+            feats = tf.reshape(style_layer_tensors[i], (-1, number))
+            gram = tf.matmul(tf.transpose(feats), feats) / size
+            style_losses.append(tf.nn.l2_loss(gram - style_gram) / size)
+        style_loss = tf.reduce_sum(style_losses)
+    with tf.name_scope("total_loss"):
+        total_loss = CONTENT_WEIGHT * content_loss + STYLE_WEIGHT * style_loss
+    optim = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(total_loss)
     init_op = tf.global_variables_initializer()
     tf.summary.FileWriter("summary/neuralstyle", graph=g2)
     with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
@@ -46,7 +65,10 @@ with tf.Graph().as_default() as g2:
         res = sess.run(content_layer_tensors)
         var = sess.run(img_tensor)
         for i in range(1000):
-            print sess.run(content_losses)
             if i % 10 == 0:
                 imsave("output-{}.jpg".format(i), sess.run(img_tensor)[0])
-            sess.run(optim)
+            _, t, c, s = sess.run([optim, total_loss, content_loss, style_loss])
+            print("total loss: {}".format(t))
+            print("content loss: {}".format(c))
+            print("style loss: {}".format(s))
+
