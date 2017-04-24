450d45e1abe3b673a12890eb67696cfbe3cf169d
sfujiwara <shuhei.fujiwara@gmail.com>
2017-04-25 01:55:09 +0900

    include_top argument

--- tfmodel/vgg.py
+++ tfmodel/vgg.py
@@ -34,7 +34,7 @@ class Vgg16:
         self.saver = None
         self._build_graph(img_tensor, reuse, trainable)
 
-    def _build_graph(self, img_tensor, reuse, trainable):
+    def _build_graph(self, img_tensor, reuse, trainable, include_top=True):
         # Convolution layers 1
         with tf.variable_scope("conv1", reuse=reuse):
             with tf.variable_scope("conv1_1"):
@@ -81,61 +81,61 @@ class Vgg16:
                 self.h_conv5_3, self.w_conv5_3, self.b_conv5_3 = _vgg_conv2d(self.h_conv5_2, 512, trainable)
         # Pooling 5
         pool5 = tf.nn.max_pool(self.h_conv5_3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME", name="pool5")
-        # Fully connected 6
-        with tf.variable_scope("fc6", reuse=reuse):
-            self.w_fc6 = tf.get_variable("weights", [7, 7, 512, 4096], tf.float32, tf.random_normal_initializer())
-            self.b_fc6 = tf.get_variable("biases", [4096], tf.float32, tf.zeros_initializer())
-            self.h_fc6 = tf.nn.relu(
-                tf.nn.bias_add(tf.nn.conv2d(pool5, self.w_fc6, strides=[1, 1, 1, 1], padding="VALID"), self.b_fc6)
-            )
-        # Fully connected 7
-        with tf.variable_scope("fc7", reuse=reuse):
-            self.w_fc7 = tf.get_variable("weights", [1, 1, 4096, 4096], tf.float32, tf.random_normal_initializer())
-            self.b_fc7 = tf.get_variable("biases", [4096], tf.float32, tf.zeros_initializer())
-            self.h_fc7 = tf.nn.relu(
-                tf.nn.bias_add(tf.nn.conv2d(self.h_fc6, self.w_fc7, strides=[1, 1, 1, 1], padding="SAME"), self.b_fc7)
-            )
-        # Fully connected 8
-        with tf.variable_scope("fc8", reuse=reuse):
-            self.w_fc8 = tf.get_variable("weights", [1, 1, 4096, 1000], tf.float32, tf.random_normal_initializer())
-            self.b_fc8 = tf.get_variable("biases", [1000], tf.float32, tf.zeros_initializer())
-            self.h_fc8 = tf.nn.bias_add(
-                tf.nn.conv2d(self.h_fc7, self.w_fc8, strides=[1, 1, 1, 1], padding="SAME"), self.b_fc8
-            )
-        with tf.variable_scope("prob"):
-            self.logits = self.h_fc8
+        self._var_dict = {
+            "vgg_16/conv1/conv1_1/weights": self.w_conv1_1,
+            "vgg_16/conv1/conv1_1/biases": self.b_conv1_1,
+            "vgg_16/conv1/conv1_2/weights": self.w_conv1_2,
+            "vgg_16/conv1/conv1_2/biases": self.b_conv1_2,
+            "vgg_16/conv2/conv2_1/weights": self.w_conv2_1,
+            "vgg_16/conv2/conv2_1/biases": self.b_conv2_1,
+            "vgg_16/conv2/conv2_2/weights": self.w_conv2_2,
+            "vgg_16/conv2/conv2_2/biases": self.b_conv2_2,
+            "vgg_16/conv3/conv3_1/weights": self.w_conv3_1,
+            "vgg_16/conv3/conv3_1/biases": self.b_conv3_1,
+            "vgg_16/conv3/conv3_2/weights": self.w_conv3_2,
+            "vgg_16/conv3/conv3_2/biases": self.b_conv3_2,
+            "vgg_16/conv3/conv3_3/weights": self.w_conv3_3,
+            "vgg_16/conv3/conv3_3/biases": self.b_conv3_3,
+            "vgg_16/conv4/conv4_1/weights": self.w_conv4_1,
+            "vgg_16/conv4/conv4_1/biases": self.b_conv4_1,
+            "vgg_16/conv4/conv4_2/weights": self.w_conv4_2,
+            "vgg_16/conv4/conv4_2/biases": self.b_conv4_2,
+            "vgg_16/conv4/conv4_3/weights": self.w_conv4_3,
+            "vgg_16/conv4/conv4_3/biases": self.b_conv4_3,
+            "vgg_16/conv5/conv5_1/weights": self.w_conv5_1,
+            "vgg_16/conv5/conv5_1/biases": self.b_conv5_1,
+            "vgg_16/conv5/conv5_2/weights": self.w_conv5_2,
+            "vgg_16/conv5/conv5_2/biases": self.b_conv5_2,
+            "vgg_16/conv5/conv5_3/weights": self.w_conv5_3,
+            "vgg_16/conv5/conv5_3/biases": self.b_conv5_3,
+        }
+        if include_top:
+            # Fully connected 6
+            with tf.variable_scope("fc6", reuse=reuse):
+                self.w_fc6 = tf.get_variable("weights", [7, 7, 512, 4096], tf.float32, tf.random_normal_initializer())
+                self.b_fc6 = tf.get_variable("biases", [4096], tf.float32, tf.zeros_initializer())
+                self.h_fc6 = tf.nn.relu(
+                    tf.nn.bias_add(tf.nn.conv2d(pool5, self.w_fc6, strides=[1, 1, 1, 1], padding="VALID"), self.b_fc6)
+                )
+            # Fully connected 7
+            with tf.variable_scope("fc7", reuse=reuse):
+                self.w_fc7 = tf.get_variable("weights", [1, 1, 4096, 4096], tf.float32, tf.random_normal_initializer())
+                self.b_fc7 = tf.get_variable("biases", [4096], tf.float32, tf.zeros_initializer())
+                self.h_fc7 = tf.nn.relu(
+                    tf.nn.bias_add(tf.nn.conv2d(self.h_fc6, self.w_fc7, strides=[1, 1, 1, 1], padding="SAME"), self.b_fc7)
+                )
+            # Fully connected 8
+            with tf.variable_scope("fc8", reuse=reuse):
+                self.w_fc8 = tf.get_variable("weights", [1, 1, 4096, 1000], tf.float32, tf.random_normal_initializer())
+                self.b_fc8 = tf.get_variable("biases", [1000], tf.float32, tf.zeros_initializer())
+                self.h_fc8 = tf.nn.bias_add(
+                    tf.nn.conv2d(self.h_fc7, self.w_fc8, strides=[1, 1, 1, 1], padding="SAME"), self.b_fc8
+                )
+            with tf.variable_scope("prob"):
+                self.logits = self.h_fc8
+            self._var_dict.update(
+                {
 
-    def restore_pretrained_variables(self, session):
-        # Create saver
-        if not self.saver:
-            self.saver = tf.train.Saver(
-                var_list={
-                    "vgg_16/conv1/conv1_1/weights": self.w_conv1_1,
-                    "vgg_16/conv1/conv1_1/biases": self.b_conv1_1,
-                    "vgg_16/conv1/conv1_2/weights": self.w_conv1_2,
-                    "vgg_16/conv1/conv1_2/biases": self.b_conv1_2,
-                    "vgg_16/conv2/conv2_1/weights": self.w_conv2_1,
-                    "vgg_16/conv2/conv2_1/biases": self.b_conv2_1,
-                    "vgg_16/conv2/conv2_2/weights": self.w_conv2_2,
-                    "vgg_16/conv2/conv2_2/biases": self.b_conv2_2,
-                    "vgg_16/conv3/conv3_1/weights": self.w_conv3_1,
-                    "vgg_16/conv3/conv3_1/biases": self.b_conv3_1,
-                    "vgg_16/conv3/conv3_2/weights": self.w_conv3_2,
-                    "vgg_16/conv3/conv3_2/biases": self.b_conv3_2,
-                    "vgg_16/conv3/conv3_3/weights": self.w_conv3_3,
-                    "vgg_16/conv3/conv3_3/biases": self.b_conv3_3,
-                    "vgg_16/conv4/conv4_1/weights": self.w_conv4_1,
-                    "vgg_16/conv4/conv4_1/biases": self.b_conv4_1,
-                    "vgg_16/conv4/conv4_2/weights": self.w_conv4_2,
-                    "vgg_16/conv4/conv4_2/biases": self.b_conv4_2,
-                    "vgg_16/conv4/conv4_3/weights": self.w_conv4_3,
-                    "vgg_16/conv4/conv4_3/biases": self.b_conv4_3,
-                    "vgg_16/conv5/conv5_1/weights": self.w_conv5_1,
-                    "vgg_16/conv5/conv5_1/biases": self.b_conv5_1,
-                    "vgg_16/conv5/conv5_2/weights": self.w_conv5_2,
-                    "vgg_16/conv5/conv5_2/biases": self.b_conv5_2,
-                    "vgg_16/conv5/conv5_3/weights": self.w_conv5_3,
-                    "vgg_16/conv5/conv5_3/biases": self.b_conv5_3,
                     "vgg_16/fc6/weights": self.w_fc6,
                     "vgg_16/fc6/biases": self.b_fc6,
                     "vgg_16/fc7/weights": self.w_fc7,
@@ -144,6 +144,11 @@ class Vgg16:
                     "vgg_16/fc8/biases": self.b_fc8,
                 }
             )
+
+    def restore_pretrained_variables(self, session):
+        # Create saver
+        if not self.saver:
+            self.saver = tf.train.Saver(var_list=self._var_dict)
         # Download weights
         save_dir = os.path.join(os.environ.get("HOME", ""), ".tfmodel", "vgg16")
         util.maybe_download_and_extract(dest_directory=save_dir, data_url=MODEL_URL)
