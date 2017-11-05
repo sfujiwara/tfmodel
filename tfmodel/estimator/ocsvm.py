038979d93ca5b073f21f7971502f1d786b237af7
sfujiwara <shuhei.fujiwara@gmail.com>
2017-11-06 02:05:36 +0900

    add evaluation to one class svm estimator

--- tfmodel/estimator/ocsvm.py
+++ tfmodel/estimator/ocsvm.py
@@ -20,6 +20,8 @@ def ocsvm_model_fn(features, labels, mode, params, config=None):
     tf.summary.histogram(name="weight", values=weight)
 
     y = tf.matmul(mapped_features, weight)
+    decision_value = y - rho
+
     with tf.name_scope("regularizer"):
         regularizer = tf.nn.l2_loss(weight)
     with tf.name_scope("hinge_loss"):
@@ -34,12 +36,22 @@ def ocsvm_model_fn(features, labels, mode, params, config=None):
         loss=loss,
         global_step=tf.train.get_global_step()
     )
+
+    # Set eval metric
+    if mode == tf.estimator.ModeKeys.EVAL:
+        eval_metric_ops = {
+            "accuracy": tf.metrics.accuracy(labels=labels, predictions=tf.sign(decision_value)),
+            "recall": tf.metrics.recall(labels=labels, predictions=tf.sign(decision_value)),
+            "precision": tf.metrics.precision(labels=labels, predictions=tf.sign(decision_value))
+        }
+    else:
+        eval_metric_ops = None
     estimator_spec = tf.estimator.EstimatorSpec(
         mode=mode,
-        predictions=y-rho,
+        predictions=decision_value,
         loss=loss,
         train_op=train_op,
-        # eval_metric_ops=eval_metric_ops,
+        eval_metric_ops=eval_metric_ops,
         # scaffold=scaffold,
         # export_outputs={"prediction": export_outputs},
     )
@@ -78,38 +90,55 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     tf.logging.set_verbosity(tf.logging.DEBUG)
-    x_normal = np.random.multivariate_normal(mean=[1., 1.], cov=np.eye(2), size=95).astype(np.float32)
-    x_outlier = np.random.multivariate_normal(mean=[10., 10.], cov=np.eye(2), size=5).astype(np.float32)
-    x = np.vstack([x_normal, x_outlier])
+    x_train = np.random.multivariate_normal(mean=[1., 1.], cov=np.eye(2), size=100).astype(np.float32)
+    x_eval = np.vstack([
+        np.random.multivariate_normal(mean=[1., 1.], cov=np.eye(2), size=950).astype(np.float32),
+        np.random.multivariate_normal(mean=[10., 10.], cov=np.eye(2), size=50).astype(np.float32)
+    ])
+    y_eval = np.array([1.]*950 + [-1.]*50).astype(np.float32)
 
     # feature_columns = [tf.feature_column.numeric_column("x", shape=[2])]
 
     config = tf.estimator.RunConfig(
-        save_summary_steps=10,
+        save_summary_steps=100,
+        save_checkpoints_steps=2000,
         model_dir="outputs"
     )
 
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": x},
+        x={"x": x_train},
         y=None,
         shuffle=True,
         batch_size=32,
-        num_epochs=5000
+        num_epochs=None
+    )
+
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": x_eval},
+        y=y_eval,
+        shuffle=False,
+        batch_size=32,
+        num_epochs=1
     )
 
     clf = OneClassSVM(
-        nu=0.3,
+        nu=0.1,
         rffm_input_dim=2,
         rffm_output_dim=2000,
         rffm_stddev=10.,
         optimizer=tf.train.ProximalAdagradOptimizer(1e-1),
         config=config
     )
-    clf.train(input_fn=train_input_fn)
+
+    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=10000)
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=eval_input_fn, start_delay_secs=0, throttle_secs=5, exporters=None
+    )
+    tf.estimator.train_and_evaluate(estimator=clf, train_spec=train_spec, eval_spec=eval_spec)
 
     predict_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": x},
-        y=None,
+        x={"x": x_eval},
+        y=y_eval,
         shuffle=False,
         num_epochs=1
     )
@@ -119,8 +148,8 @@ if __name__ == "__main__":
     threshold = 0.
     ind_normal = result > threshold
     ind_outlier = result < threshold
-    plt.plot(x[ind_normal, 0], x[ind_normal, 1], "x", label="Predicted as normal")
-    plt.plot(x[ind_outlier, 0], x[ind_outlier, 1], "x", label="Predicted as outlier")
+    plt.plot(x_eval[ind_normal, 0], x_eval[ind_normal, 1], "x", label="Predicted as normal")
+    plt.plot(x_eval[ind_outlier, 0], x_eval[ind_outlier, 1], "x", label="Predicted as outlier")
     plt.legend()
     plt.grid()
     plt.show()
